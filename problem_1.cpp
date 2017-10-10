26d918be5d23c6c5447fdf0aea6f7be54c4fad88
Sean Evans <seanevans>
2017-10-10 06:49:48 -0400

    implementations and solutions

--- problem_1.cpp
+++ problem_1.cpp
@@ -8,28 +8,7 @@
 #include <array>
 #include <algorithm>
 #include "gcd.hpp"
-#include "matrix.hpp"
 #include "inverse.hpp"
-
-// Find A, B, M, and x[11] given the following five consecutive terms in the sequence (*):
-// x[0] = 56687054115473550533
-// x[1] = 71501923691929981066
-// x[2] =  1162551557687152936
-// x[3] = 88117163952857350660
-// x[4] = 16754986973331962115
-// To help you test your answer for correctness, here is x[10]:
-// x[10]= 66775494619620378439
-
-// Please find:
-// x[11] = 70229458458028254437
-// A = 12630192673789351314
-// B = 35234390061212433526
-// M = 95034255219577607603
-
-// Hint: Use the solution of systems of two linear congruences and the gcd algorithm.
-// Simultaneous linear congruences behave just like linear equations with the one difference that you can’t divide.
-// It is a common mistake to choose M too large, which will cause some X’s to be wrong and some to be right.
-// If this happens, try dividing M by small primes such as 2 and 3.
 
 struct structured_pair {
     mpz_class coefficient;
--- problem_2.cpp
+++ problem_2.cpp
@@ -4,21 +4,53 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <chrono>
 #include <gmpxx.h>
-
-// This is only a timing test.
-// Find the smallest safe prime Psafe greater than 10150.
-// Just use Fermat’s test with 2 as a base.
-// Psafe = 10150 + k
-// k =
-// Time =
+#include "fermat_test.hpp"
 
 int main()
 {
+    // 10^150
+    mpz_class const lower_bound = 1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000_mpz;
 
-    // TODO: solution
+    // Start at the lower bound
+    // Add one because even numbers aren't prime
+    mpz_class possible_prime = lower_bound + 1;
 
-    // TODO: implement
+    // Create the corresponding prime number for safe prime testing
+    mpz_class possible_safe_prime = lower_bound / 2;
 
-    return EXIT_SUCCESS;
+    // Sample the steady clock before beginning the calculation
+    auto const start_time = std::chrono::steady_clock::now();
+
+    // Increment possible_prime by two because even numbers are not prime
+    // Increment possible_safe_prime by one to maintain the / 2 constraint
+    for ( ; true ; ++++possible_prime, ++possible_safe_prime ) {
+
+        // Test possible_prime for primality
+        if ( !fermat_test<mpz_class>( 2, possible_prime ) ) {
+            continue;
+        }
+
+        // Test possible_safe_prime for primality
+        if ( !fermat_test<mpz_class>( 2, possible_safe_prime ) ) {
+            continue;
+        }
+
+        // Sample the steady clock immediately following the calculation
+        auto const end_time = std::chrono::steady_clock::now();
+        auto const duration = end_time - start_time;
+        auto const duration_ms =
+            std::chrono::duration_cast<std::chrono::milliseconds>( duration );
+
+        // Print the answer for k
+        std::cout << "k = " << possible_prime - lower_bound << std::endl;
+
+        // Print the time in milliseconds
+        std::cout << "time = " << duration_ms.count() << " ms" << std::endl;
+
+        return EXIT_SUCCESS;
+    }
+
+    return EXIT_FAILURE;
 }
